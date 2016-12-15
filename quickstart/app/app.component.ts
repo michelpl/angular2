87dbe9b95ca747b3a4f365b529d17fd13e446eef
Michel Lima <michelpl@yahoo.com.br>
2016-12-15 11:44:18 -0200

    Styling the selection.

--- quickstart/app/app.component.ts
+++ quickstart/app/app.component.ts
@@ -6,13 +6,29 @@ export class Hero {
   
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'my-app',
   
   template: `
     <h2>My Heroes</h2>
       <ul class="heroes">
-      <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+      <li *ngFor="let hero of heroes" 
+        (click)="onSelect(hero)"
+        [class.selected]="hero === selectedHero"
+      >
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
   </ul>
@@ -75,10 +91,6 @@ export class Hero {
   }
 `]
 })
-
-
-
-
 export class AppComponent {
   
   title = 'Tour of Heroes';
@@ -91,16 +103,5 @@ export class AppComponent {
   
 }
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+
 

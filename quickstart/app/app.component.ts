eff4cb19c1175e349b4d760eed8ff286494b4ad8
Michel Lima <michelpl@yahoo.com.br>
2016-12-15 09:47:21 -0200

    Show our Hero.

--- quickstart/app/app.component.ts
+++ quickstart/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
+}

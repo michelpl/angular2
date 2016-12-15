9ac7245c1b64f9dacf8f429ebac8f9d5272858de
Michel Lima <michelpl@yahoo.com.br>
2016-12-15 10:42:07 -0200

    Listing heroes with ngFor.

--- quickstart/app/app.component.ts
+++ quickstart/app/app.component.ts
@@ -8,6 +8,12 @@ export class Hero {
 @Component({
   selector: 'my-app',
   template: `
+    <h2>My Heroes</h2>
+      <ul class="heroes">
+      <li *ngFor="let hero of heroes">
+          <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+  </ul>
   <h1>{{title}}</h1>
   <h2>{{hero.name}} details!</h2>
   <div><label>id: </label>{{hero.id}}</div>
@@ -16,10 +22,28 @@ export class Hero {
     <input [(ngModel)]="hero.name" placeholder="name">
   </div>`
 })
+
+
+
 export class AppComponent {
+  
   title = 'Tour of Heroes';
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
 }
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];

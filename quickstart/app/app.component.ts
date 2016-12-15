be1f6e0958726a42715d55f81a601931a130eabc
Michel Lima <michelpl@yahoo.com.br>
2016-12-15 10:24:59 -0200

    Two-Way Binding.

--- quickstart/app/app.component.ts
+++ quickstart/app/app.component.ts
@@ -8,14 +8,13 @@ export class Hero {
 @Component({
   selector: 'my-app',
   template: `
-      <h1>{{title}}</h1>
-      <h2>{{hero.name}} details!</h2>
-      <div>
-        <label>id: </label>{{hero.id}}
-      </div>
-      <div>
-        <label>name: </label>{{hero.name}}</div>
-    `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div>
+    <label>name: </label>
+    <input [(ngModel)]="hero.name" placeholder="name">
+  </div>`
 })
 export class AppComponent {
   title = 'Tour of Heroes';

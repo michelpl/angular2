3e76daeedabe3662b4b1888922e82200627399bd
Michel Lima <michelpl@yahoo.com.br>
2016-12-15 09:56:01 -0200

    Hero object.

--- quickstart/app/app.component.ts
+++ quickstart/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+  template: `
+      <h1>{{title}}</h1>
+      <h2>{{hero.name}} details!</h2>
+      <div>
+        <label>id: </label>{{hero.id}}
+      </div>
+      <div>
+        <label>name: </label>{{hero.name}}</div>
+    `
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
